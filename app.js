bff58244249a580589faef3439288b303aa6477b
bluyam <bluyam@tamu.edu>
2016-01-16 13:37:19 -0600

    debug

--- app.js
+++ app.js
@@ -153,9 +153,7 @@ function getUserId(feed) {
 function displayFeed() {
   var feed;
   /* make the API call */
-  FB.api("/me/feed?fields=from,message&limit=50", {
-        "with": "location"
-      }
+  FB.api("/me/feed?fields=from,message&limit=50", 
       ,function (response) {
         if (response && !response.error) {
           console.log("logging data");
